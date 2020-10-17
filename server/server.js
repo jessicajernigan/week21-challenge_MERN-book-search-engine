23aaa35ac303ffe5dc99f66b72403f4ba3a0eb5c
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-17 14:23:07 -0500

    trucking along - added typeDefs & resolvers

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,8 +7,21 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+// Important Imports
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+// integrate  Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -15,6 +29,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
